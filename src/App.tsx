df7e9a5384f5d2b42e287d7598553bf5c0749e72
Sankaku <sankaku_dlt.45631@outlook.jp>
2020-08-14 08:45:14 +0900

    Format code with prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 const { myAPI } = window;
 
@@ -37,9 +37,7 @@ function App() {
         >
           Learn React
         </a>
-        <p>
-          {text}
-        </p>
+        <p>{text}</p>
       </header>
     </div>
   );
