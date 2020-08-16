795fca1d8de3969cc86670dd43af0ee1dce832b0
Sankaku <sankaku_dlt.45631@outlook.jp>
2020-08-16 12:15:05 +0900

    Set background color

--- electron/main.ts
+++ electron/main.ts
@@ -8,6 +8,7 @@ function createWindow() {
   const win = new BrowserWindow({
     width: 800,
     height: 600,
+    backgroundColor: "#121212",
     webPreferences: {
       nodeIntegration: false,
       contextIsolation: true,
